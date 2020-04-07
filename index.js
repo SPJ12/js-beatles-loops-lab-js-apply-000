28b24a21254665e6b2997ed433ce8ad128ac41e7
Sandeep Jain <skj786@gmail.com>
2020-04-07 00:02:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ theBeatlesPlay(musicians, instruments);
   for(var i=0;i<musicians.length; i++)
   {
     var temp2 = musicians[i] + instruments[i];
-    temp1.pus
+    temp1.push(temp2);
   }
-  
+  return temp1
 }
